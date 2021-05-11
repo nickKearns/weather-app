40afb2e6f3d05dc9a84cda0c80d3b6fd36b60200
Mohammad Azam <azamsharp@gmail.com>
2021-05-11 10:01:06 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mohammad Azam on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
